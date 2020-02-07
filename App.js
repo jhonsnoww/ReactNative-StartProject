f5eb502c757c2243c7038c41a44dcd4481a83dac
jhonsnoww <oochanbg@gmail.com>
2020-02-07 12:07:04 +0630

    start State

--- App.js
+++ App.js
@@ -2,38 +2,58 @@ import React, { useState } from 'react';
 import { StyleSheet, Text, View, Button, TextInput } from 'react-native';
 
 export default function App() {
+
+  const [enteredWords,setEntereWords] = useState('');
+  const [worldList,setWorldList] = useState([]);
+
+  const wordHandler=(e)=>{
+    setEntereWords(e)
+  }
+
+  const addWordToList =()=>{
+    setWorldList(currentWords=>[...currentWords,enteredWords])
+  }
  
   return (
-  
-    <View style={{padding:30 , flexDirection:'row',width:'80%',}}>
 
-     <View style={{
-       height:100,
-       flex:1,
-       backgroundColor:'blue',
-       justifyContent:'space-between',//MainAxisAligment
-       alignItems:'stretch'//CrossAxisAligment
-       }}> 
-        <Text>1</Text>
-     </View>
-     <View style={{
-        height:100,
-        backgroundColor:'green',
-       justifyContent:'space-between',
-       alignItems:'baseline'}}> 
-       <Text>2</Text>
-     </View>
-     <View style={{
-        height:100,
-        backgroundColor:'red',
-       justifyContent:'center',
-       alignItems:'baseline'}}> 
-       <Text>3</Text>
-     </View>
+
+    <View style={{padding:50}}> 
+
+<View style={styles.screen}>
+    <TextInput 
+      placeholder="Enter World"  
+      style={styles.inputStyle} 
+      onChangeText={wordHandler}
+      value={enteredWords}
+      />
+
+   <Button 
+   title="ADD" 
+   onPress={addWordToList}
+    style={{
+     padding:30,
+    }}/>
+    </View>
+    <View>
+      {
+        
+      worldList.map((word)=><Text>{word}</Text>)
+      
+      }
+    </View>
     </View>
   );
-}
-
+  }
 const styles = StyleSheet.create({
- 
+  screen: {
+    padding:50,
+    flexDirection:'row'
+    
+  },
+  inputStyle:{
+    width:'80%',
+    borderWidth:1, 
+    borderColor:'black',
+    borderStyle:'solid',
+  }
 });
